7358f791a58b8ec9818b184a05ead90e98c1f4e6
Nikolas Nyby <nikolas@gnu.org>
2022-10-20 16:56:36 -0400

    Remove django auth stuff
    I don't think we'll be using this.

--- mathplayground/settings.py
+++ mathplayground/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'channels',
     'mathplayground',
     'smoketest',
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -54,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -70,7 +67,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
